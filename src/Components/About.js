54d3b18cb382bd4e5b876c71c765e7e690ab9deb
Axedeep14 <deepakkpaliwal@gmail.com>
2019-11-16 20:34:41 +0530

    Edited ProfilePage

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,8 +10,9 @@ class About extends Component {
                   <div class="col-lg-6 col-md-12">
                     <div class="card-body">
                       <div class="h4 mt-0 title">About</div>
-                      <p>Enthusiastic Web App Developer and Android App Developer eagerly looking for opportunities to contribute to team success through hard work, attention to detail and organizational skills.</p>
-                      <p> Motivated to learn and grow in this industry <a href="https://www.linkedin.com/in/axedeep14/" target="_blank">Learn More</a></p>
+                      <p>Enthusiastic Web and App Developer eagerly looking for opportunities to contribute to team success through hard work, attention to detail and organizational skills.</p>
+                      <p> Motivated to learn and grow in this industry.</p>
+                      <p><a href="https://www.linkedin.com/in/axedeep14/" target="_blank">Learn More</a></p>
                     </div>
                   </div>
                   <div class="col-lg-6 col-md-12">
@@ -19,24 +20,24 @@ class About extends Component {
                       <div class="h4 mt-0 title">Basic Information</div>
                       <div class="row">
                         <div class="col-sm-4"><strong class="text-uppercase">Age:</strong></div>
-                        <div class="col-sm-8">24</div>
+                        <div class="col-sm-8">20</div>
                       </div>
                       <div class="row mt-3">
                         <div class="col-sm-4"><strong class="text-uppercase">Email:</strong></div>
-                        <div class="col-sm-8">anthony@company.com</div>
+                        <div class="col-sm-8">deepakkpaliwal@gmail.com</div>
                       </div>
                       <div class="row mt-3">
                         <div class="col-sm-4"><strong class="text-uppercase">Phone:</strong></div>
-                        <div class="col-sm-8">+1718-111-0011</div>
+                        <div class="col-sm-8">+918769449558</div>
                       </div>
                       <div class="row mt-3">
                         <div class="col-sm-4"><strong class="text-uppercase">Address:</strong></div>
-                        <div class="col-sm-8">140, City Center, New York, U.S.A</div>
+                        <div class="col-sm-8">B-64,Flat No.F-2, Shankar Vihar-B,Siddarth Nagar,Jaipur,Rajasthan,India-302017</div>
                       </div>
-                      <div class="row mt-3">
+                      {/* <div class="row mt-3">
                         <div class="col-sm-4"><strong class="text-uppercase">Language:</strong></div>
-                        <div class="col-sm-8">English, German, French</div>
-                      </div>
+                        <div class="col-sm-8">Hindi,English</div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
